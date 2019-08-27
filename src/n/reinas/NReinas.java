8006b3bb313c32dcdb54a2cc53f900fd302d6bc0
Mvillegas1995 <50243838+Mvillegas1995@users.noreply.github.com>
2019-08-27 16:49:43 -0400

    Población

--- src/n/reinas/NReinas.java
+++ src/n/reinas/NReinas.java
@@ -1,24 +1,39 @@
 package n.reinas;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class NReinas {
 
     public static void main(String[] args) {
         
         int tampob, reinas;
-        tampob = 20;
+        tampob = 8;
         reinas = 8;
         
         int [][] poblacion = new int [tampob][reinas];
+        ArrayList<Integer> aux = new ArrayList<>();
         
-        int k = 0;
-        for (int i = 0; i < 20; i++) {
-            k = 0;
-            for (int j = 1; j < 9; j++) {
-                poblacion[i][j] = k;
+        for (int i = 0; i < tampob; i++) {
+            for (int j = 1; j < reinas+1; j++) {
+                poblacion[i][j-1] = j;
             }            
         }
-        for (int i = 0; i < 20; i++) {
-                        
+        Random r = new Random();
+        for (int i = 0; i < tampob; i++) {
+            for (int j=0; j<reinas; j++) {
+                int posAleatoria = r.nextInt(reinas);
+                int temp = poblacion[i][j];
+                poblacion[i][j] = poblacion[i][posAleatoria];
+                poblacion[i][posAleatoria] = temp;
+            }
+        }
+        
+        for (int i = 0; i < tampob; i++) {
+            for (int j = 0; j < reinas; j++) {
+                System.out.print(poblacion[i][j]+" ");
+            }
+            System.out.println("");
         }
     }
     
