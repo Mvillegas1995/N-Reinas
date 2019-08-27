e37dbcbc00df8ff722db21ae096dbf00f0ab6f9b
Mvillegas1995 <50243838+Mvillegas1995@users.noreply.github.com>
2019-08-27 16:08:11 -0400

    Update NReinas.java

--- src/n/reinas/NReinas.java
+++ src/n/reinas/NReinas.java
@@ -4,6 +4,22 @@ public class NReinas {
 
     public static void main(String[] args) {
         
+        int tampob, reinas;
+        tampob = 20;
+        reinas = 8;
+        
+        int [][] poblacion = new int [tampob][reinas];
+        
+        int k = 0;
+        for (int i = 0; i < 20; i++) {
+            k = 0;
+            for (int j = 1; j < 9; j++) {
+                poblacion[i][j] = k;
+            }            
+        }
+        for (int i = 0; i < 20; i++) {
+                        
+        }
     }
     
 }
