57ffc22ba76dc37dc454ce362faf94b02b0a0f2e
Mvillegas1995 <50243838+Mvillegas1995@users.noreply.github.com>
2019-08-28 19:04:37 -0400

    Modularizado
    Se modularizo lo que llevamos

--- src/n/reinas/GeneticAlg.java
+++ src/n/reinas/GeneticAlg.java
@@ -1,6 +1,73 @@
 
 package n.reinas;
 
+import java.util.Random;
+
 public class GeneticAlg {
     
+    int tampob;
+    int reinas;
+    int [][] poblacion;
+    int [] fitness;
+    
+    public GeneticAlg(int tampob, int reinas){
+        
+        this.tampob = tampob;
+        this.reinas = reinas;
+        
+        poblacion = new int [tampob][reinas];
+        for (int i = 0; i < tampob; i++) {
+            for (int j = 0; j < reinas; j++) {
+                poblacion[i][j] = j;
+            }            
+        }
+        
+        fitness = new int [tampob];
+        for (int i = 0; i < tampob; i++) {
+            fitness[i] = 0;
+        }
+      
+    }
+    
+    public void desordenarTablero(){    
+        Random r = new Random();
+        for (int i = 0; i < tampob; i++) {
+            for (int j=0; j<reinas; j++) {
+                int posAleatoria = r.nextInt(reinas);
+                int temp = poblacion[i][j];
+                poblacion[i][j] = poblacion[i][posAleatoria];
+                poblacion[i][posAleatoria] = temp;
+            }
+        }
+        
+    }
+    
+    public void imprimirPoblacion(){   
+        for (int i = 0; i < tampob; i++) {
+            for (int j = 0; j < reinas; j++) {
+                System.out.print(poblacion[i][j]+" ");
+            }
+            System.out.println("");
+        }  
+    }
+    
+    public void calcularFitness(){
+        for (int k = 0; k < tampob; k++) {
+            // k itera entre poblaciones
+            for (int i = 0; i < reinas-1; i++) {
+                // i itera el indice 
+                for (int j = i+1; j < reinas; j++) {
+                    //j itera el hacia la derecha del tablero
+                    if(Math.abs(j-i) == Math.abs(poblacion[k][j]-poblacion[k][i]))
+                        fitness[k]++;
+                }         
+            }     
+        }
+    }
+    
+    public void imprimirFitness(){
+        for (int i = 0; i < tampob; i++) {
+            System.out.println(fitness[i]);
+        }
+    }
 }
--- src/n/reinas/NReinas.java
+++ src/n/reinas/NReinas.java
@@ -1,65 +1,20 @@
 package n.reinas;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 public class NReinas {
 
     public static void main(String[] args) {
         
-        int tampob, reinas;
-        tampob = 10;
-        reinas = 8;
+        int tampob = 10;
+        int reinas = 8;
         
-        // ASIGNAR POBLACION
-        int [][] poblacion = new int [tampob][reinas];    
-        for (int i = 0; i < tampob; i++) {
-            for (int j = 0; j < reinas; j++) {
-                poblacion[i][j] = j;
-            }            
-        }
+        GeneticAlg x = new GeneticAlg(tampob, reinas);
         
-        // INICIALIZAR FITNESS
-        int [] fitness = new int [tampob];
-        for (int i = 0; i < tampob; i++) {
-            fitness[i] = 0;
-        }
+        x.desordenarTablero();
+        x.imprimirPoblacion();
         
-        Random r = new Random();
-        for (int i = 0; i < tampob; i++) {
-            for (int j=0; j<reinas; j++) {
-                int posAleatoria = r.nextInt(reinas);
-                int temp = poblacion[i][j];
-                poblacion[i][j] = poblacion[i][posAleatoria];
-                poblacion[i][posAleatoria] = temp;
-            }
-        }
-        
-        for (int i = 0; i < tampob; i++) {
-            for (int j = 0; j < reinas; j++) {
-                System.out.print(poblacion[i][j]+" ");
-            }
-            System.out.println("");
-        }
-        
-        //CALCULAR FITNESS
-        for (int k = 0; k < tampob; k++) {
-            // k itera entre poblaciones
-            for (int i = 0; i < reinas-1; i++) {
-                // i itera el indice 
-                for (int j = i+1; j < reinas; j++) {
-                    //j itera el hacia la derecha del tablero
-                    if(Math.abs(j-i) == Math.abs(poblacion[k][j]-poblacion[k][i]))
-                        fitness[k]++;
-                }
-                
-            }
-            
-        }
-        //IMPRIME EL FITNESS
-        for (int i = 0; i < tampob; i++) {
-            System.out.println(fitness[i]);
-        }
+        x.calcularFitness();
+        x.imprimirFitness();
+
     }
     
 }
